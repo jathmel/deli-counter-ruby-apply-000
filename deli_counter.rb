86b49519b9c6d5be1c9b6f52a59235969be2e006
Jathmel Gordon <jathmel@gmail.com>
2018-04-24 23:12:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+# Write your code here_s
+def line(array)
+  if array.empty?
+    puts "The line is currently empty."
+  else
+    new_str = ''
+    i = 0
+    while i < array.length 
+      new_str << " #{i+1}. #{array[i]}"
+      i += 1
+    end
+    puts 'The line is currently:' << new_str 
+  end
+end
+
+# def take_a_number(array, name)
+#   array << name
+#   puts "Welcome, #{name}. You are number #{array.index(name) + 1} in line."
+#   array
+# end
+num = 0
+def take_a_number(array)
+  puts "Welcome, you are number: #{num++}"
+  array << num
+end
+
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{array.shift}."
+  end
+end
